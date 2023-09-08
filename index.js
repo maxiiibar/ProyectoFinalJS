1149ea58b0184d36d9ad18306ff12eec16120661
Maximo <maximobar2003@gmail.com>
2023-09-08 13:01:27 -0300

    Busqueda completada

--- index.js
+++ index.js
@@ -1,6 +1,99 @@
-const personajes = document.querySelector(".personajes");
-const episodios = document.querySelector(".episodios");
-const locaciones = document.querySelector(".locaciones");
+const personajes = document.querySelector(".personajes")
+const episodios = document.querySelector(".episodios")
+const locaciones = document.querySelector(".locaciones")
+const mainIndex = document.querySelector("main")
+const presentacion = document.querySelector("#presentacion")
+
+const barraBusqueda = document.querySelector("#barraDeBusqueda")
+const inputBusqueda = document.querySelector("#input-busqueda")
+barraBusqueda.addEventListener("submit",(event)=>{
+	event.preventDefault()
+	presentacion.innerHTML = ""
+	mainIndex.innerHTML = `
+		<section class="encontrados container">
+			<h3>Personajes</h3>
+			<div class="boxPersonajes"></div>
+		</section>
+		<section class="encontrados container">
+			<h3>Episodios</h3>
+			<div class="boxEpisodios"></div>
+		</section>
+		<section class="encontradas container">
+			<h3>Locaciones</h3>
+			<div class="boxLocaciones"></div>
+		</section>
+	`
+	presentacion.classList.remove("presentacion");
+	presentacion.classList.add("nuevoHeight")
+	
+	const contenedorPersonajes = document.querySelector(".boxPersonajes")
+	const contenedorEpisodios = document.querySelector(".boxEpisodios")
+	const contenedorLocaciones = document.querySelector(".boxLocaciones")
+	
+	
+	fetch (`https://rickandmortyapi.com/api/character/?name=${inputBusqueda.value.toLowerCase()}`)
+	.then ( res => res.json() )
+	.then ( data => {
+		if(data.error){
+			contenedorPersonajes.innerHTML = `
+            <div class="vacio">
+                <h3>No se encontraron personajes con este nombre</h3>
+            </div>  
+            `
+		}
+		else{
+			
+			if (data.results.length===1){
+				cardsAHtml(data.results[0], singlePCard, contenedorPersonajes)
+			}
+			else{
+				cardsAHtml(data.results,cardsPersonajes,contenedorPersonajes)
+			}
+		}
+	})
+	
+	fetch (`https://rickandmortyapi.com/api/episode/?name=${inputBusqueda.value.toLowerCase()}`)
+	.then ( res => res.json() )
+	.then ( data => {
+		if(data.error){
+			contenedorEpisodios.innerHTML = `
+            <div class="vacio">
+                <h3>No se encontraron episodios con este nombre</h3>
+            </div>  
+            `
+		}
+		else{
+			if (data.results.length===1){
+				cardsAHtml(data.results[0], singleECard, contenedorEpisodios)
+			}
+			else{
+				cardsAHtml(data.results,cardsEpisodios,contenedorEpisodios)
+			}
+		}
+	})
+	
+	fetch (`https://rickandmortyapi.com/api/location/?name=${inputBusqueda.value.toLowerCase()}`)
+	.then ( res => res.json() )
+	.then ( data => {
+		if(data.error){
+			contenedorLocaciones.innerHTML = `
+            <div class="vacio">
+                <h3>No se encontraron locaciones con este nombre</h3>
+            </div>  
+            `
+		}
+		else{
+			if (data.results.length===1){
+				cardsAHtml(data.results[0], singleLCard, contenedorLocaciones)
+			}
+			else{
+				cardsAHtml(data.results,cardsLocaciones,contenedorLocaciones)
+			}
+		}
+	})
+
+
+})
 
 const cardsPersonajes = data => {
     const card = data.reduce((acc, element) => {
@@ -24,6 +117,27 @@ const cardsPersonajes = data => {
     return card
 }
 
+const singlePCard = element => {
+    const singleP = `
+    <div class="card-personaje">
+        <div class="imagen-personaje">
+            <img src="${element.image || "imagen-no-disponible.png"}">
+        </div>
+        <div class="contenido-personaje">
+            <h5>${ element.name || "Nombre no disponible"}</h5>
+            <p>Especie: ${element.species || "No disponible"}</p>
+            <p>Genero: ${element.genre || "No disponible"}</p>
+            <p>Estado: ${element.status || "No disponible"}</p>
+        </div>
+        <div class="fav-personaje">
+            <button class="boton-eliminar boton-personaje" id="del-${element.id}"><i class="fa-solid fa-trash fa-lg"></i></i></button>
+        </div>
+    </div>
+`
+return singleP
+}
+
+
 const cardsEpisodios = data => {
     const card = data.reduce((acc, element) => {
         return acc + `
@@ -42,6 +156,22 @@ const cardsEpisodios = data => {
     return card
 }
 
+const singleECard = element => {
+    const singleE = `
+    <div class="card-episodio">
+        <div class="contenido-episodio">
+            <h5>${ element.name || "Nombre no disponible"}</h5>
+            <p>Episodio: ${element.episode || "No disponible"}</p>
+            <p>Fecha de emisión: ${element.air_date || "No disponible"}</p>
+        </div>
+        <div class="fav-episodio">
+            <button class="boton-eliminar boton-episodio" id="del-${element.id}"><i class="fa-solid fa-trash fa-lg"></i></button>
+        </div>
+    </div>
+`
+return singleE
+}
+
 const cardsLocaciones = data => {
     const card = data.reduce((acc, element) => {
         return acc + `
@@ -58,6 +188,22 @@ const cardsLocaciones = data => {
         `
     },"")
     return card
+}
+
+const singleLCard = element => {
+    const singleL = `
+    <div class="card-locacion">
+        <div class="contenido-locacion">
+            <h5>${ element.name || "Nombre no disponible"}</h5>
+            <p>Tipo: ${element.type || "No disponible"}<p>
+            <p>Dimensión: ${element.dimension || "No disponible"}</p>
+        </div>
+        <div class="fav-locacion">
+            <button class="boton-eliminar boton-locacion" id="del-${element.id}"><i class="fa-solid fa-trash fa-lg"></i></i></button>
+        </div>
+    </div>
+`
+return singleL
 }
 
 const cardsAHtml = (data, funcion, contenedor) => {
@@ -148,4 +294,6 @@ fetch (`https://rickandmortyapi.com/api/location`)
     cardsAHtml(data.results.slice(0,6), cardsLocaciones, locaciones)
     agregarAFav(".boton-locacion","locaciones-fav")
 })
+
+
 
